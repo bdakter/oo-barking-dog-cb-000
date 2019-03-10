7a8c99493595d09ed9aa24811789a8786d18d65f
Boruch Z <philomath613@gmail.com>
2019-03-10 16:04:01 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,26 @@
 
-foo = [1, 2, 3, 4, 5, 6, 6, 6, nil]
+class TicTacToe
+  def initialize(board = nil)
+    @board = board || Array.new(9, " ")
+  end
 
-def count(foo)
-  foo.count{1} #{|num| num % 2 == 0}
+ 
+  def current_player
+    turn_count % 2 == 0 ? "X" : "O"
+  end
+ 
+  def turn_count
+    @board.count{|token| token == "X" || token == "O"}
+  end
+ 
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
 end
 
-p count(foo)
+game = TicTacToe.new
+game.display_board
